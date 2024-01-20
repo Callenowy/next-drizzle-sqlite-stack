f1350861a4fcdb01f7c3163dd38aa767a2550442
Radoslaw Napora <radoslaw.napora@outlook.com>
2024-01-21 00:04:32 +0100

    feat: add Text component and tailwind class mappings

---
+++ src/styles/classMappings.ts
@@ -0,0 +1,54 @@
+export const colorMap = {
+  'grey-200': 'text-grey-200',
+  'grey-300': 'text-grey-300',
+  'grey-500': 'text-grey-500',
+  'grey-600': 'text-grey-600',
+  'neutral-300': 'text-neutral-300',
+  'neutral-700': 'text-neutral-700',
+  'neutral-800': 'text-neutral-800',
+  'neutral-950': 'text-neutral-950',
+  'blue-50': 'text-blue-50',
+  'blue-400': 'text-blue-400',
+  'blue-500': 'text-blue-500',
+  'blue-600': 'text-blue-600',
+  'blue-700': 'text-blue-700',
+  'red-500': 'text-red-500',
+  'red-600': 'text-red-600',
+  'red-700': 'text-red-700',
+  black: 'text-black',
+} as const;
+
+export const sizeMap = {
+  xs: 'text-xs',
+  sm: 'text-sm',
+  base: 'text-base',
+  md: 'text-md',
+  lg: 'text-lg',
+  xl: 'text-xl',
+  '2xl': 'text-2xl',
+} as const;
+
+export const weightMap = {
+  thin: 'font-thin',
+  extralight: 'font-extralight',
+  light: 'font-light',
+  normal: 'font-normal',
+  medium: 'font-medium',
+  semibold: 'font-semibold',
+  bold: 'font-bold',
+  extrabold: 'font-extrabold',
+  black: 'font-black',
+} as const;
+
+export const spacingMap = {
+  tightest: 'tracking-tightest',
+  tighter: 'tracking-tighter',
+  tight: 'tracking-tight',
+  thin: 'tracking-thin',
+} as const;
+
+export const alignMap = {
+  left: 'text-left',
+  center: 'text-center',
+  right: 'text-right',
+} as const;
