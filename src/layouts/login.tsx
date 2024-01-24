438018660f424a98c1e50c86d477fe67644fc876
Radoslaw Napora <radoslaw.napora@outlook.com>
2024-01-24 01:32:32 +0100

    feat(login): add login layout

---
+++ src/layouts/login.tsx
@@ -0,0 +1,5 @@
+export const LoginLayout = ({ children }: React.PropsWithChildren) => (
+  <main className="flex h-full min-h-screen w-full items-center justify-center p-4 md:p-8">
+    {children}
+  </main>
+);
