bfc53fb9b05210015444cfb6fbc3883f71a260d2
Radoslaw Napora <radoslaw.napora@outlook.com>
2024-01-25 14:54:57 +0100

    feat(app-page): add login form, loading state and error handling

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,16 +1,64 @@
-import type { Metadata } from 'next';
-import { LoginLayout } from '@/layouts/login';
-import { LoginCard } from '@/components/loginCard';
+'use client';
 
-export const metadata: Metadata = {
-  title: 'Login',
-  description: 'Login to your account on the Stack App',
-};
+import { useState } from 'react';
+
+import { LoginCard } from '@/components/loginCard';
+import { Heading } from '@/components/heading';
+import { Text } from '@/components/text';
+import { Loader } from '@/components/loader';
+import { LoginForm } from '@/components/loginForm';
+import { Alert } from '@/components/alert';
+
+import type { LoginSchema } from '@/models';
 
 export default function LoginPage() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(
+    undefined
+  );
+
+  const handleSubmit = (data: LoginSchema) => {
+    try {
+      setErrorMessage(undefined);
+      setIsLoading(true);
+      console.log(data);
+      //await handleLogin(data);
+    } catch (error: unknown) {
+      setErrorMessage('Unable to authenticate with the given credentials.');
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <LoginLayout>
-      <LoginCard>Login Soon!</LoginCard>
-    </LoginLayout>
+    <LoginCard>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <header className="pb-8">
+            <Heading
+              level="1"
+              order="2"
+              className="text-center text-lg md:text-xl"
+            >
+              Welcome back!
+            </Heading>
+            <Text
+              weight="medium"
+              color="grey-600"
+              align="center"
+              size="sm"
+              className="pt-4 md:text-base"
+            >
+              Enter details below to log in to your account.
+            </Text>
+          </header>
+
+          {errorMessage && <Alert message={errorMessage} />}
+
+          <LoginForm onSubmit={handleSubmit} />
+        </>
+      )}
+    </LoginCard>
   );
 }
