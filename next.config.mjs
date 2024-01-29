547c60db0e5c6fdb1ec0d5d7b7a41fa7ff41c97e
Radoslaw Napora <radoslaw.napora@outlook.com>
2024-01-29 21:21:42 +0100

    feat(next.config): add experimental server components and redirect rules

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,31 @@ const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   output: 'standalone',
+
+  experimental: {
+    serverComponentsExternalPackages: ['@node-rs/argon2'],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/server-list',
+        permanent: true,
+      },
+      {
+        source: '/auth/login',
+        destination: '/server-list',
+        permanent: true,
+        has: [
+          {
+            type: 'cookie',
+            key: 'authjs.session-token',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
